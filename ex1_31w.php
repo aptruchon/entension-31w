3445600312ddaa7ac3fb3f1060b346be654f0ff2
aptruchon <aptruchon@gmail.com>
2022-06-02 11:40:10 -0400

    s3-c2 Configuration de notre premier plugin

---
+++ ex1_31w.php
@@ -0,0 +1,8 @@
+<?php
+    /**
+     * Plugin Name: Extension de l'exercice #1
+     * Author: Alex Poulin Truchon
+     * Description: Une première extension pour l'exercice #1
+     * Author URI: https://github.com/aptruchon
+     */ 
+?>
