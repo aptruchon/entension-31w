85a7825ca32aa0aaf8d0c12871c1a9172dc20d71
aptruchon <aptruchon@gmail.com>
2022-06-02 15:03:23 -0400

    plugin ex1

--- ex1_31w.php
+++ ex1_31w.php
@@ -3,6 +3,17 @@
      * Plugin Name: Extension de l'exercice #1
      * Author: Alex Poulin Truchon
      * Description: Une première extension pour l'exercice #1
-     * Author URI: https://github.com/aptruchon
+     * Author URI: https://github.com/aptruchon/31w-extension1.git
      */ 
+?>
+
+<?php
+function fonction_generale(){
+    function mon_31w_scripts() {
+        wp_enqueue_style( 'mon_31w-style', get_stylesheet_uri(), array(), _S_VERSION );
+        
+    }
+    add_action( 'wp_enqueue_scripts', 'mon_31w_scripts' );
+}
+    add_action("init", "generale");
 ?>
